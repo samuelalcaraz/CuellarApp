e588900e1c3a55bbb6e27d58b28252acf332005e
Samuel Alcaraz <salcara@almaximoti.com>
2020-12-18 06:01:48 -0600

    Obtener Localización

--- app/src/main/java/com/compuasis/censoalumbradopublico/MainActivity.java
+++ app/src/main/java/com/compuasis/censoalumbradopublico/MainActivity.java
@@ -18,6 +18,7 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
